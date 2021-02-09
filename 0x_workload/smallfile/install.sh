103a2f5d32e29764998f0e9baf8e29b413d228ee
Jan Peschke <jan.peschke@quobyte.com>
2021-02-09 11:40:33 +0100

    reduce file count because of small nvmes

--- 0x_workload/smallfile/install.sh
+++ 0x_workload/smallfile/install.sh
@@ -1,6 +1,6 @@
-git clone https://github.com/distributed-system-analysis/smallfile.git
+# Ensure python3 is available on every involved host :)
 
-# import a new policy
+# import a smallfile policy
 qmgmt policy-rule import quobyte-smallfile-policy.quo
 
 # tag devices with tag "fast":
@@ -9,5 +9,14 @@ for device in $devicelist; do
 done
 
 # create a volume for testing
-qmgt volume ...
+qmgmt volume create testvolume myuser mygroup BASE
 
+# create test directory:
+
+mkdir -p /quobyte/testvolume/smallfile-test
+git clone https://github.com/distributed-system-analysis/smallfile.git /quobyte/testvolume/smallfile
+
+export myhosts=$(grep ^registry /etc/quobyte/host.cfg | sed 's/registry=//g')
+
+# do a distributed smallfile test
+python3 /quobyte/testvolume/smallfile/smallfile_cli.py --operation create --threads 3 --file-size 2048 --files 10240 --top /quobyte/testvolume/smallfile-test/ --host-set ${myhosts}
