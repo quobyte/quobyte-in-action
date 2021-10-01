5c42d4d64119ca6ed564a514882e54cfd8845e55
Jan Peschke <jan@corp.quobyte.com>
2021-10-01 15:07:05 +0200

    improve stepping

--- 0x_workload/genereate_different_filesizes.sh
+++ 0x_workload/genereate_different_filesizes.sh
@@ -2,14 +2,15 @@
 
 # generate files of different sizes.
 
-filenumber=1000
-stepping=1
-upper_border=20
-size=1; 
+filenumber=10000	# how many files are generated
+stepping=10		# steps to increment file sizes
+upper_border=200	# how many steps are incremented before we start again
+count=1; 		# lets start with a minimal file size
+bs=32k			# actual file sizes are count * bs
 
-for i in $(seq 1000) ; do 
-	dd if=/dev/zero of=file-$i bs=1M count=$size; ((size++)); 
-	if [[ "$size" -ge "20" ]] ; then 
-		size=1; 
-	fi ;
+for i in $(seq ${filenumber}) ; do
+	dd if=/dev/zero of=file-$i bs=${bs} count=$count; count=$((count+stepping)); 
+        if [[ "$count" -ge "${upper_border}" ]] ; then
+                count=1; 
+        fi ;
 done
