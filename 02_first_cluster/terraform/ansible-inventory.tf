132a44d1ff726e0907aa65f14c19cd119f464d73
Jan Peschke <jan.peschke@quobyte.com>
2022-04-21 11:50:59 +0200

    rename inventory file to contain .yaml ending

--- 02_first_cluster/terraform/ansible-inventory.tf
+++ 02_first_cluster/terraform/ansible-inventory.tf
@@ -8,9 +8,9 @@ resource "local_file" "AnsibleInventory" {
   client_ip = join(":\n", google_compute_instance.client.*.network_interface.0.network_ip)
  }
  )
- filename = "provisioning/ansible-inventory"
+ filename = "provisioning/ansible-inventory.yaml"
  file_permission = "0644"
  provisioner "local-exec" {
-   command = "until scp provisioning/ansible-inventory deploy@${google_compute_instance.core.0.network_interface.0.access_config.0.nat_ip}: ; do sleep 1 ; done"
+   command = "until scp provisioning/ansible-inventory.yaml deploy@${google_compute_instance.core.0.network_interface.0.access_config.0.nat_ip}: ; do sleep 1 ; done"
  }
 }
