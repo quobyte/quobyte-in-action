c5d1bf3754b56e4607df80ccab9665618e46d889
Jenny MacDonald <jenny@quobyte.com>
2023-07-31 10:38:55 +0200

    dns changes

--- 02_first_cluster/terraform/99_extension_dns.tf
+++ 02_first_cluster/terraform/99_extension_dns.tf
@@ -13,7 +13,16 @@ resource "google_dns_record_set" "registry" {
   managed_zone = var.dns_zone
   type         = "A"
   ttl          = 300
-  rrdatas = [google_compute_instance.core[count.index].network_interface.0.network_ip]
+  rrdatas = [google_compute_instance.core[count.index].network_interface.1.network_ip]
+}
+
+resource "google_dns_record_set" "registry-frontend" {
+  count        = var.number_coreserver
+  name         = "registry-frontend${count.index}.${var.dns_domain}"
+  managed_zone = var.dns_zone
+  type         = "A"
+  ttl          = 300
+  rrdatas = [google_compute_instance.core[count.index].network_interface.2.network_ip]
 }
 
 resource "google_dns_record_set" "console" {
@@ -58,5 +67,17 @@ resource "google_dns_record_set" "registry-srv" {
     "0 0 7861 ${google_dns_record_set.registry[0].name}",
     "0 0 7861 ${google_dns_record_set.registry[1].name}",
     "0 0 7861 ${google_dns_record_set.registry[2].name}"
+  ]
+}
+
+resource "google_dns_record_set" "registry-frontend-srv" {
+  name = "_quobyte._tcp.frontend.quobyte-demo.com."
+  type = "SRV"
+  ttl  = 60
+  managed_zone = var.dns_zone
+  rrdatas = [
+    "0 0 7861 ${google_dns_record_set.registry-frontend[0].name}",
+    "0 0 7861 ${google_dns_record_set.registry-frontend[1].name}",
+    "0 0 7861 ${google_dns_record_set.registry-frontend[2].name}"
   ]
 }
