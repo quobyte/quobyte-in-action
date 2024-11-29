59b0df9308686141baf46cc17caae8fd1161427d
Jan Peschke <jan.peschke@quobyte.com>
2024-11-29 12:11:12 +0100

    Add DNS subdomains for grafana/prometheus

---
+++ 03_trending/terraform/99_extension_dns.tf
@@ -0,0 +1,18 @@
+
+resource "google_dns_record_set" "prometheus" {
+  name         = "prometheus.quobyte-demo.com."
+  managed_zone = "quobyte-demo-shard-1"
+  type         = "A"
+  ttl          = 300
+  rrdatas = google_compute_instance.trending.*.network_interface.0.access_config.0.nat_ip
+}
+
+resource "google_dns_record_set" "grafana" {
+  name         = "grafana.quobyte-demo.com."
+  managed_zone = "quobyte-demo-shard-1"
+  type         = "A"
+  ttl          = 300
+  rrdatas = google_compute_instance.trending.*.network_interface.0.access_config.0.nat_ip
+}
+
+
