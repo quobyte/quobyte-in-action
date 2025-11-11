f94e97a50b0379ec8a9eb25088d497210ebe65f9
Jan Peschke <jan.peschke@quobyte.com>
2025-11-11 10:39:45 +0100

    Also add a machine dns entry

--- 03_trending/terraform/99_extension_dns.tf
+++ 03_trending/terraform/99_extension_dns.tf
@@ -7,6 +7,15 @@ resource "google_dns_record_set" "prometheus" {
   rrdatas = google_compute_instance.trending.*.network_interface.0.access_config.0.nat_ip
 }
 
+resource "google_dns_record_set" "prometheus2" {
+  name         = "trending0.quobyte-demo.com."
+  managed_zone = "quobyte-demo-shard-1"
+  type         = "A"
+  ttl          = 300
+  rrdatas = google_compute_instance.trending.*.network_interface.0.access_config.0.nat_ip
+}
+
+
 resource "google_dns_record_set" "grafana" {
   name         = "grafana.quobyte-demo.com."
   managed_zone = "quobyte-demo-shard-1"
